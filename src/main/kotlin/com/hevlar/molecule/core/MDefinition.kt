158340f4c1ae7711cc90dbfb5798439b1b9e30b6
Dennis Cai <dennis.cai@zuhlke.com>
2021-10-23 09:58:43 +0800

    update definition

--- src/main/kotlin/com/hevlar/molecule/core/MDefinition.kt
+++ src/main/kotlin/com/hevlar/molecule/core/MDefinition.kt
@@ -1,16 +1,45 @@
 package com.hevlar.molecule.core
 
-class MDefinition(name: String, type: MType): MType(name, MText, {
+import kotlin.reflect.typeOf
+
+/*
+Definition is in the format { name: type } where name is the identifier of this definition
+and type must be a subtype of MType. e.g. { name: MText } or { age: MNumber }
+ */
+class MDefinition(name: String, val type: MType): MType(name, MMap, {
     val defaultPackage = "com.hevlar.molecule.core"
     try {
         val isMap = MMap.test(it) == MMap
         val map = MMap.parse(it)!!
-        val mapContainsNameAndType = map.keys.containsAll(listOf("name", "type"))
-        val givenType = map["type"].toString()
+        val givenType = map.values.first().toString();
         val fqn = if (givenType.contains(".")) givenType else "${defaultPackage}.${givenType}"
-        val testTypeExists = Class.forName(fqn).kotlin.objectInstance != null
-        isMap && mapContainsNameAndType && testTypeExists
+        val typeInstance = Class.forName(fqn).kotlin.objectInstance
+        isMap && map.count() == 1 && typeInstance != null && typeInstance is MType
     }catch (e: Throwable){
         false
     }
-})
+}){
+
+    override fun parse(value: String): MDefinition? {
+        val defaultPackage = "com.hevlar.molecule.core"
+        return try {
+            val isMap = MMap.test(value) == MMap
+            val map = MMap.parse(value)!!
+            val givenType = map.values.first().toString();
+            val fqn = if (givenType.contains(".")) givenType else "${defaultPackage}.${givenType}"
+            val typeInstance = Class.forName(fqn).kotlin.objectInstance
+            if(map.count() == 1 && typeInstance != null && typeInstance is MType){
+                MDefinition(map.keys.first(), typeInstance)
+            }else{
+                null
+            }
+        }catch (e: Throwable){
+            null
+        }
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if(other !is MDefinition) return false
+        return this.name == other.name && this.type == other.type
+    }
+}
--- src/test/kotlin/com/hevlar/molecule/core/MDefinitionTest.kt
+++ src/test/kotlin/com/hevlar/molecule/core/MDefinitionTest.kt
@@ -2,15 +2,133 @@ package com.hevlar.molecule.core
 
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
+import kotlin.test.expect
 
 internal class MDefinitionTest{
 
     @Test
-    fun `test MDefinition ok`(){
+    fun `test MDefinition MText ok`(){
         val json = """
-            { "name": "Name", "type": "MText" }
+            { "name": "MText" }
         """.trimIndent()
         val expected = MDefinition("Name", MText)
         assertEquals(expected, expected.test(json))
+    }
+
+    @Test
+    fun `test MDefinition MBoolean ok`(){
+        val json = """
+            { "Registered": "MBoolean" }
+        """.trimIndent()
+        val expected = MDefinition("Registered", MBoolean)
+        assertEquals(expected, expected.test(json))
+    }
+
+    @Test
+    fun `test MDefinition MNumber ok`(){
+        val json = """
+            { "Years of Experience": "MNumber" }
+        """.trimIndent()
+        val expected = MDefinition("Years of Experience", MNumber)
+        assertEquals(expected, expected.test(json))
+    }
+
+    @Test
+    fun `test MDefinition MInteger ok`(){
+        val json = """
+            { "Age": "MInteger" }
+        """.trimIndent()
+        val expected = MDefinition("Age", MInteger)
+        assertEquals(expected, expected.test(json))
+    }
+
+    @Test
+    fun `test MDefinition mismatch should fail`(){
+        val json = """
+            { "Name: "MText" }
+        """.trimIndent()
+        val expected = MDefinition("Age", MInteger)
+        assertNotEquals(expected, expected.test(json))
+    }
+
+    @Test
+    fun `test MDefinition multiple values should fail`(){
+        val json = """
+            { "Name: "MText", "Age", "MInteger" }
+        """.trimIndent()
+        val expected = MDefinition("Age", MInteger)
+        assertNotEquals(expected, expected.test(json))
+    }
+
+    @Test
+    fun `test MDefinition invalid MType should fail`(){
+        val json = """
+            { "Name: "Text" }
+        """.trimIndent()
+        val expected = MDefinition("Name", MText)
+        assertNotEquals(expected, expected.test(json))
+    }
+
+    @Test
+    fun `parse MDefinition MText ok`(){
+        val json = """
+            { "Name": "MText" }
+        """.trimIndent()
+        val expected = MDefinition("Name", MText)
+        assertEquals(expected, expected.parse(json))
+    }
+
+    @Test
+    fun `parse MDefinition MBoolean ok`(){
+        val json = """
+            { "Registered": "MBoolean" }
+        """.trimIndent()
+        val expected = MDefinition("Registered", MBoolean)
+        assertEquals(expected, expected.parse(json))
+    }
+
+    @Test
+    fun `parse MDefinition MNumber ok`(){
+        val json = """
+            { "Years of Experience": "MNumber" }
+        """.trimIndent()
+        val expected = MDefinition("Years of Experience", MNumber)
+        assertEquals(expected, expected.parse(json))
+    }
+
+    @Test
+    fun `parse MDefinition MInteger ok`(){
+        val json = """
+            { "Age": "MInteger" }
+        """.trimIndent()
+        val expected = MDefinition("Age", MInteger)
+        assertEquals(expected, expected.parse(json))
+    }
+
+    @Test
+    fun `parse MDefinition mismatch should return null`(){
+        val json = """
+            { "Name: "MText" }
+        """.trimIndent()
+        val expected = MDefinition("Age", MInteger)
+        assertNull(expected.parse(json))
+    }
+
+    @Test
+    fun `parse MDefinition multiple values should return null`(){
+        val json = """
+            { "Name: "MText", "Age", "MInteger" }
+        """.trimIndent()
+        val expected = MDefinition("Age", MInteger)
+        assertNull(expected.parse(json))
+    }
+
+    @Test
+    fun `parse MDefinition invalid MType should fail`(){
+        val json = """
+            { "Name: "Text" }
+        """.trimIndent()
+        val expected = MDefinition("Name", MText)
+        assertNull(expected.parse(json))
     }
 }
