ce73359365eb6f695619789fa611f650b921721e
Dennis Cai <dennis.cai@zuhlke.com>
2021-10-25 17:45:58 +0800

    add property

---
+++ src/main/kotlin/com/hevlar/molecule/core/MProperty.kt
@@ -0,0 +1,37 @@
+package com.hevlar.molecule.core
+
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+
+/*
+MProperty defines the properties for a class, with the following attributes
+1. type
+2. required - default true
+3. default - optional
+ */
+object MProperty : MType("MProperty", MMap, {
+    val testMap = try {
+        val type = object : TypeToken<Map<String, String>>(){}.type
+        Gson().fromJson<Map<String, String>>(it, type)
+    }catch (e: Throwable){
+        throw e
+    }
+
+    if (!testMap.containsKey("type")) throw Throwable("Invalid Property: type is not defined in property declaration")
+    if (MDefinition.test(testMap["type"]!!) != MDefinition) throw Throwable("Invalid Property: type is not a valid MDefinition")
+    if (testMap.containsKey("required") && MBoolean.test(testMap["required"]!!) != MBoolean) {
+        throw Throwable("Invalid Property: required field is not a valid boolean")
+    }
+    if (testMap.containsKey("default")){
+        try {
+            val typeInstance = getInstance(testMap["type"]!!)
+            if (typeInstance.test(testMap["default"]!!) == typeInstance) throw Throwable("Invalid Property: default value does not pass test of property type")
+        }catch (e: Throwable){
+            throw Throwable("Invalid Property: Error with the default")
+        }
+    }
+
+    true
+}){
+    
+}
--- src/test/kotlin/com/hevlar/molecule/core/MMethodTest.kt
+++ src/test/kotlin/com/hevlar/molecule/core/MMethodTest.kt
@@ -6,6 +6,20 @@ import org.junit.jupiter.api.Test
 internal class MMethodTest {
 
     @Test
+    fun `test method should return MMap if invalid`(){
+        val json = """
+            {
+                "inputs": {
+                    "a": "Number",
+                    "b": "MNumber"
+                },
+                "output": "MNumber"
+            }
+        """.trimIndent()
+        assertEquals(MMap, MMethod.test(json))
+    }
+
+    @Test
     fun testAddMethod(){
         val json = """
             {
