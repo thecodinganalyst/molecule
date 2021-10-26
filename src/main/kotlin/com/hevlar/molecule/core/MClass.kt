9c4114435f222e0108fc6dd41e8a542f95572066
Dennis Cai <dennis.cai@zuhlke.com>
2021-10-26 16:57:11 +0800

    update mclass

--- src/main/kotlin/com/hevlar/molecule/core/MClass.kt
+++ src/main/kotlin/com/hevlar/molecule/core/MClass.kt
@@ -15,13 +15,8 @@ object MClass: MType("MClass", Data, { value ->
         null
     }
 }){
-    override fun parse(value: String): Map<String, MType>? {
-        return try {
-            val type = object : TypeToken<Map<String, String>>(){}.type
-            Gson().fromJson<Map<String, String>>(value, type)
-                .mapValues { getInstance(it.value) }
-        }catch (e: Throwable){
-            null
-        }
+    override fun parse(value: String): Map<String, MType?>? {
+        val tmp = this.parseFunction(value) as Map<*, *>?
+        return tmp?.mapKeys { it.key as String }?.mapValues { it.value as MType }
     }
 }
